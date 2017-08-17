069108e63b34c67a74c77659303535e1a74e9919
rbaselio@gmail.com <darth10>
2017-08-17 20:18:58 -0300

    Tratando evento de click

---
+++ app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
@@ -0,0 +1,27 @@
+package com.robertolopes.agenda;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
