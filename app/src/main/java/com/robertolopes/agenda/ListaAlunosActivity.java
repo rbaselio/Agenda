db17106595a40d33377e7fc4178c0ff558314b1b
rbaselio@gmail.com <darth10>
2017-08-17 19:35:27 -0300

    Melhorando a Lista

--- app/src/main/java/com/robertolopes/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/robertolopes/agenda/ListaAlunosActivity.java
@@ -1,13 +1,9 @@
 package com.robertolopes.agenda;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -15,6 +11,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Roberto", "Marcos", "Tadeu", "Dunha"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+
+
     }
 
 
