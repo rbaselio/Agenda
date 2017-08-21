d5d1f43b137f1f85b3b8c13b8edfbe1669e30952
rbaselio@gmail.com <darth10>
2017-08-21 20:19:52 -0300

    Utilizando menus

--- app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
+++ app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
@@ -2,8 +2,9 @@ package com.robertolopes.agenda;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
@@ -13,16 +14,25 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
