34c579167e13179f6fc08fa4a4ac9f21984cc4d4
rbaselio@gmail.com <darth10>
2017-08-22 20:53:16 -0300

    Recuperando dados do aluno

--- app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
+++ app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
@@ -7,14 +7,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.robertolopes.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-
+        helper = new FormularioHelper(this);
     }
 
     @Override
@@ -28,7 +31,9 @@ public class FormularioActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_formulario_ok:
-                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_SHORT).show();
+                Aluno aluno = helper.pegaAluno();
+                Toast.makeText(FormularioActivity.this, "Aluno: " + aluno.getNome() + " Salvo", Toast.LENGTH_SHORT).show();
+
                 finish();
                 break;
         }
