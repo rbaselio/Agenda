f0da1e42a75c40c0049dc4898a4703e86a2d6d35
rbaselio@gmail.com <darth10>
2017-08-24 20:13:58 -0300

    Invocando a câmera

--- app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
+++ app/src/main/java/com/robertolopes/agenda/FormularioActivity.java
@@ -1,15 +1,22 @@
 package com.robertolopes.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.robertolopes.agenda.dao.AlunoDAO;
 import com.robertolopes.agenda.modelo.Aluno;
+
+import java.io.File;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -25,6 +32,19 @@ public class FormularioActivity extends AppCompatActivity {
         if (aluno != null) {
             helper.preencheFormulario(aluno);
         }
+
+        Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                Uri uri = FileProvider.getUriForFile(FormularioActivity.this, BuildConfig.APPLICATION_ID + ".provider", arquivoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivity(intentCamera);
+            }
+        });
 
     }
 
